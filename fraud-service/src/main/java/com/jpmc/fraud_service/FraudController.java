461f5fb2b537bee66fa23161286cfb71c2ea6c09
SarahPerrotta <sarahperrotta2000@hotmail.co.uk>
2026-05-25 16:55:34 -0400

    day-09: add Swagger API docs to all three services

--- fraud-service/src/main/java/com/jpmc/fraud_service/FraudController.java
+++ fraud-service/src/main/java/com/jpmc/fraud_service/FraudController.java
@@ -1,16 +1,20 @@
 package com.jpmc.fraud_service;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/fraud")
+@Tag(name = "Fraud Detection", description = "Rule-based fraud detection endpoints")
 public class FraudController {
 
     @Autowired
     private FraudRuleEngine fraudRuleEngine;
 
+    @Operation(summary = "Check a transaction for fraud risk")
     @PostMapping("/check")
     public ResponseEntity<FraudCheckResponse> checkFraud(
             @RequestBody FraudCheckRequest request) {
